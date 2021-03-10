1f779f22c75477dd723332d4202d4e2e318af791
furkanakkulak <74897847+furkanakkulak@users.noreply.github.com>
2021-03-10 20:40:18 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,10 +1,12 @@
 List1 = [1,3,5,7,9]
 List2 = [2,4,6,8]
-MergeList = List1 + List2
-print("Merge List: ", MergeList)
-MergeList.sort()
-print("Sort Merge List: ", MergeList)
-for i in range (len(MergeList)):
-    MergeList[i]*=2
-print("Merge List * 2: ", MergeList)
-print("Data Type: ",type(MergeList))
+MergedList = List1 + List2
+print("Merged List: ", MergedList)
+MergedList.sort()
+print("Sort Merged List: ", MergedList)
+for i in range (len(MergedList)):
+    MergedList[i]*=2
+print("Merged List * 2: ", MergedList)
+print("Veri Tipi: ",type(MergedList))
+for i in MergedList:
+  print("Data type of the",i//2,"element",type(i))
