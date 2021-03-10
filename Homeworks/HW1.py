ede0f9b8b9b3f65bb8d15caa24d20085a28b3e92
furkanakkulak <74897847+furkanakkulak@users.noreply.github.com>
2021-03-10 14:30:38 +0300

    Add files via upload

---
+++ Homeworks/HW1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+List1 = [1,3,5,7,9]
+List2 = [2,4,6,8]
+MergeList = List1 + List2
+print("Merge List: ", MergeList)
+MergeList.sort()
+print("Sort Merge List: ", MergeList)
+for i in range (len(MergeList)):
+    MergeList[i]*=2
+print("Merge List * 2: ", MergeList)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
