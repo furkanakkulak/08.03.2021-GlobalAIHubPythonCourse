e757dae14b63aced956ef53d0ca679c797d81862
Furkan <74897847+furkanakkulak@users.noreply.github.com>
2021-03-13 22:20:00 +0300

    Add files via upload

---
+++ Homeworks/HW3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+ogrenciler={}
+for i in range(1,3):
+    print("")
+    print("Öğrenci",i,"Bilgileri")
+    print("---------------------")
+    adsoyad = str(input("Öğrencinin Adı Soyadı: "))
+    asinav = int(input("Öğrencinin Ara Sınavı: "))
+    pnotu = int(input("Öğrencinin Proje Notu: "))
+    fnotu = int(input("Öğrencinin Final Notu: "))
+    gnotu = (asinav * (0.3)) + (pnotu * (0.3)) + (fnotu * (0.4))
+    ogrenciler[i] = {"Ad Soyad": adsoyad, "Ara Sınav": str(asinav),"Proje Notu":str(pnotu),"Final Notu":str(fnotu),"Geçme Notu":str(gnotu)}
+sirali_ogrenciler = sorted(ogrenciler.items(), key=lambda x: float(x[1]["Geçme Notu"]),reverse=True)
+print(sirali_ogrenciler)
+
+
+# In[ ]:
+
+
+
+
